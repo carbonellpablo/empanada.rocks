4e381ce1541f0262a85268e5bd7678a0fb42efc3
Pablo Carbonell <pablocarbonell@icloud.com>
2021-10-01 00:32:52 +0300

    chore: deploy script

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  const Empanada = await hre.ethers.getContractFactory("Empanada");
+  const empanada = await Empanada.deploy();
+  await empanada.deployed();
+  console.log("empanada deployed to:", empanada.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
