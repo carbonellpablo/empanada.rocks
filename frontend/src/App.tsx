715bd0b3f1408370b075f81cc802f1afa97f04af
Pablo Carbonell <pablocarbonell@icloud.com>
2021-10-01 15:42:29 +0300

    feat: styling improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import { ethers } from 'ethers';
-import { NFTfactory} from './config';
+import { NFTfactory } from './config';
 
 function App(): JSX.Element {
+  const [txId, setTxId] = useState();
+  const [error, setError] = useState();
   const handleClick = (e: React.FormEvent) => {
     e.preventDefault();
     mintNFT();
@@ -37,8 +40,37 @@ function App(): JSX.Element {
 
   return (
     <div className="App">
-      <img src="./base.png" />
-      <button onClick={handleClick}>Get your free Empanada!</button>
+      <h1>empanda.rocks</h1>
+      <h2>An NFT collection featuring 12 empanadas</h2>
+      <img src="./base.png" width="400px" height="400px" />
+
+      {txId ? (
+        <p>
+          Congratulations! your NFT was transfer. You can see the transaction
+          here:{' '}
+        </p>
+      ) : (
+        <button onClick={handleClick}>Get your free Empanada!</button>
+      )}
+
+      <div className="content">
+        <h2>About</h2>
+        <p>Total supply: 12 NFT</p>
+        <p>Max mint per address: 1 NFT</p>
+        <p>
+          <a href="https://github.com/carbonellpablo/empanada.rocks">
+            Github repository
+          </a>
+        </p>
+      </div>
+
+      <div className="content">
+        <h2>Instructions</h2>
+        <p>Only works on desktop, not mobile.</p>
+        <p>You need to have the browser extension Metamask installed.</p>
+        <p>Make sure you are on the &apos;Rinkeby&apos; network.</p>
+        <p>Click on the button above!</p>
+      </div>
     </div>
   );
 }
